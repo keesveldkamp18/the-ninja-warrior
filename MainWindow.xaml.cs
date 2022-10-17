26fab12b62c51264992941239a02c7c93a976a18
berend109 <berend109@gmail.com>
2022-10-17 13:12:01 +0200

    Pauze functie toegevoegd.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,41 +15,41 @@ using System.Windows.Shapes;
 
 namespace project_arcade
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
-		private void one_player_button(object sender, RoutedEventArgs e)
-		{
-			_1spelerkeuze GW = new _1spelerkeuze();
-			GW.Visibility = Visibility.Visible;
-			this.Close();
-		}
+        private void one_player_button(object sender, RoutedEventArgs e)
+        {
+            _1spelerkeuze GW = new _1spelerkeuze();
+            GW.Visibility = Visibility.Visible;
+            this.Close();
+        }
 
-		private void two_player_button(object sender, RoutedEventArgs e)
-		{
-			_2spelerkeuze gw = new _2spelerkeuze();
-			gw.Visibility = Visibility.Visible;
-			this.Close();
-		}
+        private void two_player_button(object sender, RoutedEventArgs e)
+        {
+            _2spelerkeuze gw = new _2spelerkeuze();
+            gw.Visibility = Visibility.Visible;
+            this.Close();
+        }
 
-		private void Highscores_button(object sender, RoutedEventArgs e)
-		{
-			HighscoresWindow GW = new HighscoresWindow();
-			GW.Visibility = Visibility.Visible;
-			this.Close();
-		}
+        private void Highscores_button(object sender, RoutedEventArgs e)
+        {
+            HighscoresWindow GW = new HighscoresWindow();
+            GW.Visibility = Visibility.Visible;
+            this.Close();
+        }
 
-		private void Quit_button(object sender, RoutedEventArgs e)
-		{
-			Application.Current.Shutdown();
-		}
+        private void Quit_button(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
 
-	}
+    }
 }
