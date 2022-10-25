80a0a19882369cfdb2094dd1eb5e2eb7408313d6
Sil <silvanderkooi@gmail.com>
2022-10-25 13:15:25 +0200

    Revert some changes of "Added background parallax and did code refactoring"

--- GameWindow.xaml.cs
+++ GameWindow.xaml.cs
@@ -1,33 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using static System.Windows.MessageBoxResult;
 
 namespace project_arcade
 {
+	/// <summary>
+	/// Interaction logic for GameWindow.xaml
+	/// </summary>
 	public partial class GameWindow : Window
 	{
-		#region Variables
 		DispatcherTimer timer = new DispatcherTimer();
 
 		int speed = 10;
-		int gravity;
+		int gravity = 0;
 		double lastPlayerTop;
 		bool onFloor;
-		#endregion
 
 		public GameWindow()
 		{
 			InitializeComponent();
 
-			InitializeGameWindow();
-		}
-
-		private void InitializeGameWindow()
-		{
+			gameCanvas.Focus();
 			timer.Tick += GameEngine;
 			timer.Interval = TimeSpan.FromMilliseconds(20);
 			timer.Start();
@@ -35,73 +40,43 @@ namespace project_arcade
 
 		private void GameEngine(object? sender, EventArgs e)
 		{
-			PlayerMovement();
-
-			PlayerGravity();
-
-			PlayerCollisionDetection();
-
-			PlayerScreenBoundsDetection();
-
-			PauseChecking();
-
-			BackgroundParallax();
-		}
-
-		private void PlayerScreenBoundsDetection()
-		{
-			// Keep the player within view
-			if(Canvas.GetLeft(Player) < 0)
-			{
-				Canvas.SetLeft(Player, 0);
-			}
-			else if(Canvas.GetLeft(Player) > 1525)
-			{
-				Canvas.SetLeft(Player, 1525);
-			}
-		}
-
-		private void PlayerMovement()
-		{
-			// Move left if the left arrow key is held and/or right if the right arrow key is held
-			if(Keyboard.IsKeyDown(Key.Left))
+			#region player move controls
+			// Horizontal movement
+			if (Keyboard.IsKeyDown(Key.Left))
 			{
 				Canvas.SetLeft(Player, Canvas.GetLeft(Player) - speed);
 			}
-
-			if(Keyboard.IsKeyDown(Key.Right))
+			if (Keyboard.IsKeyDown(Key.Right))
 			{
 				Canvas.SetLeft(Player, Canvas.GetLeft(Player) + speed);
 			}
 
-			// Adds negative force if the spacebar is pressed
-			if(Keyboard.IsKeyDown(Key.Space) && onFloor)
+			// Jumping
+			if (Keyboard.IsKeyDown(Key.Space) && onFloor)
 			{
 				gravity = -20;
 				onFloor = false;
 			}
-		}
+			#endregion
 
-		private void PlayerGravity()
-		{
+			#region Player Gravity
 			// Makes the player fall down
 			lastPlayerTop = Canvas.GetTop(Player);
 			gravity++;
 			Canvas.SetTop(Player, Canvas.GetTop(Player) + gravity);
-		}
+			#endregion
 
-		private void PlayerCollisionDetection()
-		{
+			#region player collision detection
 			Rect playerRect = new(Canvas.GetLeft(Player), Canvas.GetTop(Player), Player.Width, Player.Height);
 
-			foreach(var rectangle in gameCanvas.Children.OfType<Rectangle>())
+			foreach (var rectangle in gameCanvas.Children.OfType<Rectangle>())
 			{
-				if((string)rectangle.Tag == "Platform")
+				if ((string)rectangle.Tag == "Platform")
 				{
 					Rect platformRect = new(Canvas.GetLeft(rectangle), Canvas.GetTop(rectangle), rectangle.Width, rectangle.Height);
 
-					// Don't let the player fall through a platform if the player was above or on it the previous tick
-					if(playerRect.IntersectsWith(platformRect) && lastPlayerTop + Player.Height <= Canvas.GetTop(rectangle))
+					// Don't let the player fall through the platform iff the player was above or on the platform the previous tick
+					if (playerRect.IntersectsWith(platformRect) && lastPlayerTop + Player.Height <= Canvas.GetTop(rectangle))
 					{
 						gravity = 0;
 						Canvas.SetTop(Player, Canvas.GetTop(rectangle) - Player.Height);
@@ -109,33 +84,32 @@ namespace project_arcade
 					}
 				}
 			}
-		}
+			#endregion
 
-		private void BackgroundParallax()
-		{
-			var backgroundSegments = gameCanvas.Children.OfType<Image>().Where(i => (string)i.Tag == "Background").ToArray();
-
-			// Move each segment a little left to simulate depth
-			for(int i = 0; i < backgroundSegments.Length; i++)
+			#region player screen bounds detection
+			// if the player goes too far left or right it puts them back
+			if (Canvas.GetLeft(Player) < 0)
 			{
-				var backgroundSegment = backgroundSegments[i];
-				Canvas.SetLeft(backgroundSegment, (Canvas.GetLeft(backgroundSegment) - 0.5 * (i + 1)) % 4400);
+				Canvas.SetLeft(Player, 0);
 			}
-		}
+			else if (Canvas.GetLeft(Player) > 1525)
+			{
+				Canvas.SetLeft(Player, 1525);
+			}
+			#endregion
 
-		private void PauseChecking()
-		{
-			// If P was pressed...
-			if(!Keyboard.IsKeyDown(Key.P)) return;
+			#region pause function
+			if (!Keyboard.IsKeyDown(Key.P)) return;
 			const string caption = "PAUZE";
-			const string message = "Wil je stoppen met spelen?";
+			const string message = "Wil je ophouden met spelen ??";
 			const MessageBoxButton buttons = MessageBoxButton.YesNo;
-
-			// ...show a window and return to the main menu if Yes was clicked
-			if(MessageBox.Show(message, caption, buttons) != MessageBoxResult.Yes) return;
+			if (MessageBox.Show(message, caption, buttons) != Yes) return;
+			// OK code
 			MainWindow mainWindow = new();
 			mainWindow.Show();
 			Close();
+			#endregion
 		}
+
 	}
 }
