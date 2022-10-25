747eef9cca2ec9c8029a0a21d1dd9fd6f9f92dce
Sil <silvanderkooi@gmail.com>
2022-10-25 13:10:40 +0200

    Revert "Code refactoring"
    This reverts commit c5d4df15c6a9953ea57a4ef48f33517e831d5b94.

--- GameWindow.xaml.cs
+++ GameWindow.xaml.cs
@@ -11,12 +11,12 @@ namespace project_arcade
 	public partial class GameWindow : Window
 	{
 		#region Variables
-		private readonly DispatcherTimer timer = new();
+		DispatcherTimer timer = new DispatcherTimer();
 
-		private const int speed = 10;
-		private int gravity;
-		private double lastPlayerTop;
-		private bool onFloor;
+		int speed = 10;
+		int gravity;
+		double lastPlayerTop;
+		bool onFloor;
 		#endregion
 
 		public GameWindow()
