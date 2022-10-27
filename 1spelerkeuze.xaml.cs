e9beec3ea04ab05e6b3826f59ac13440aba23958
Sil <silvanderkooi@gmail.com>
2022-10-27 13:03:55 +0200

    Fixed variable regions

--- 1spelerkeuze.xaml.cs
+++ 1spelerkeuze.xaml.cs
@@ -2,28 +2,28 @@
 
 namespace project_arcade
 {
-    /// <summary>
-    /// Interaction logic for _1spelerkeuze.xaml
-    /// </summary>
-    public partial class _1spelerkeuze : Window
-    {
-        public _1spelerkeuze()
-        {
-            InitializeComponent();
-        }
+	/// <summary>
+	/// Interaction logic for _1spelerkeuze.xaml
+	/// </summary>
+	public partial class _1spelerkeuze : Window
+	{
+		public _1spelerkeuze()
+		{
+			InitializeComponent();
+		}
 
-        private void OnePlayerToMain(object sender, RoutedEventArgs e)
-        {
-            MainWindow gw = new MainWindow();
-            gw.Visibility = Visibility.Visible;
-            this.Close();
-        }
+		private void OnePlayerToMain(object sender, RoutedEventArgs e)
+		{
+			MainWindow gw = new MainWindow();
+			gw.Visibility = Visibility.Visible;
+			this.Close();
+		}
 
-        private void OnePlayerToGame(object sender, RoutedEventArgs e)
-        {
-            GameWindow gw = new GameWindow(false);
-            gw.Visibility = Visibility.Visible;
-            this.Close();
-        }
-    }
+		private void OnePlayerToGame(object sender, RoutedEventArgs e)
+		{
+			GameWindow gw = new GameWindow(false);
+			gw.Visibility = Visibility.Visible;
+			this.Close();
+		}
+	}
 }
--- HighscoresWindow.xaml.cs
+++ HighscoresWindow.xaml.cs
@@ -6,57 +6,57 @@ using System.Windows.Controls;
 
 namespace project_arcade
 {
-    /// <summary>
-    /// Interaction logic for HighscoresWindow.xaml
-    /// </summary>
-    public partial class HighscoresWindow : Window
-    {
+	/// <summary>
+	/// Interaction logic for HighscoresWindow.xaml
+	/// </summary>
+	public partial class HighscoresWindow : Window
+	{
 
-        //might need to download and reference the connector. Download: https://dev.mysql.com/downloads/connector/net/ 
-        string connectionString = "SERVER=web0113.zxcs.nl,3306; DATABASE=u45926p46412_highscore; UID=u45926p46412_highscore; PASSWORD=ninjaEndlessRunner;";
+		//might need to download and reference the connector. Download: https://dev.mysql.com/downloads/connector/net/ 
+		string connectionString = "SERVER=web0113.zxcs.nl,3306; DATABASE=u45926p46412_highscore; UID=u45926p46412_highscore; PASSWORD=ninjaEndlessRunner;";
 
-        public HighscoresWindow()
-        {
-            InitializeComponent();
+		public HighscoresWindow()
+		{
+			InitializeComponent();
 
-            //Connect to DB
-            MySqlConnection connection = new MySqlConnection(connectionString);
+			//Connect to DB
+			MySqlConnection connection = new MySqlConnection(connectionString);
 
-            //open connection
-            connection.Open();
+			//open connection
+			connection.Open();
 
-            //Retreive scores from DB
-            MySqlCommand cmd = new MySqlCommand("SELECT * FROM scores ORDER BY playerScore DESC LIMIT 20", connection);
+			//Retreive scores from DB
+			MySqlCommand cmd = new MySqlCommand("SELECT * FROM scores ORDER BY playerScore DESC LIMIT 20", connection);
 
-            DataTable dt = new DataTable();
-            dt.Load(cmd.ExecuteReader());
-            connection.Close();
+			DataTable dt = new DataTable();
+			dt.Load(cmd.ExecuteReader());
+			connection.Close();
 
-            dt.Columns.Add("rank", typeof(String));
+			dt.Columns.Add("rank", typeof(String));
 
-            foreach(DataRow row in dt.Rows)
-            {
-                row.SetField("rank", dt.Rows.IndexOf(row) + 1 + ".");
-            }
+			foreach(DataRow row in dt.Rows)
+			{
+				row.SetField("rank", dt.Rows.IndexOf(row) + 1 + ".");
+			}
 
-            highscoreList.DataContext = dt;
-        }
-        //TODO: database legen toevoegen
-        private void Leeg_Database(object sender, RoutedEventArgs e)
-        {
+			highscoreList.DataContext = dt;
+		}
+		//TODO: database legen toevoegen
+		private void Leeg_Database(object sender, RoutedEventArgs e)
+		{
 
-        }
+		}
 
-        private void Terug_Naar_Startscherm(object sender, RoutedEventArgs e)
-        {
-            MainWindow gw = new MainWindow();
-            gw.Visibility = Visibility.Visible;
-            this.Close();
-        }
+		private void Terug_Naar_Startscherm(object sender, RoutedEventArgs e)
+		{
+			MainWindow gw = new MainWindow();
+			gw.Visibility = Visibility.Visible;
+			this.Close();
+		}
 
-        private void highscoreList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+		private void highscoreList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
 
-        }
-    }
+		}
+	}
 }
